5b9f729932f9d7b54004ce1915d603bb4858565f
Jakub Ahaddad <j.ahaddad@protonmail.com>
2017-10-19 19:33:04 +0200

    minor fixes

--- kol1.py
+++ kol1.py
@@ -21,7 +21,7 @@
 #	with st.dev of 30 deg.
 #fix will be applied analogously, with the mean at -angle and st.dev of 5.0
 
-import random
+from random import gauss
 from time import sleep
 
 print("Keyboard Interrupt (CTRL+C) to stop.")
@@ -31,14 +31,16 @@ i = 1
 
 while True:
 	print("Starting step {0:d}\nInitial angle: {1:2.3f} deg.".format(i,angle))
-	tilt = random.gauss(0.0,30.0)
+	tilt = gauss(0.0,30.0)
 	angle += tilt
 	print( "Tilting by {0:2.3f} deg.\nCurrent angle: {1:2.3f}".format(tilt,angle) )
-	fix = random.gauss(-angle,5.0)	#centered around minus current angle
+	fix = gauss(-angle,5.0)	#centered around minus current angle
 									#so it already gives a compensating value
 	print( "Applying fixing tilt of {:2.3f} deg.".format(fix) )
 	angle += fix
 	print( "Angle at the end of the step: {:2.3f}\n".format(angle) )
+
+	print("Reminder: To stop the program, please do a keyboard interrupt (CTRL+C)")
 	i += 1
 	sleep(2)
 
